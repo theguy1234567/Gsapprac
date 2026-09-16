40ebf37003f5bd0acb17fa52ecd29f5240bf513a
theguy1234567 <kappanna@gitam.in>
2026-09-16 23:17:42 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+function App() {
+  return <div className="flex-center h-[100vh] ">App</div>;
+}
+
+export default App;
