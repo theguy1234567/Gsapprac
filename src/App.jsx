41f133a1bead8a75f864337553dcda4cf6050e63
theguy1234567 <kappanna@gitam.in>
2026-09-24 23:19:21 +0530

    added Art section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 gsap.registerPlugin(ScrollTrigger);
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Navbar />
       <Hero />
       <Cocktails />
-      <About/>
+      <About />
+      <Art />
     </main>
   );
 }
